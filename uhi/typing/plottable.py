66b4b23033577c7fc5ae10ee4f2c1ea8f54732a6
Henry Schreiner <henryschreineriii@gmail.com>
2020-12-01 11:48:47 -0500

    refactor: update from recent meeting
    Counts is now explicitly "effective", Interpretation->Kind and now uppercase strings.

--- uhi/typing/plottable.py
+++ uhi/typing/plottable.py
@@ -28,16 +28,19 @@ from typing import runtime_checkable
 # from numpy.typing import ArrayLike # requires unreleased NumPy 1.20
 ArrayLike = Iterable[float]
 
-# Known kinds of histograms. A Producer can add types not defined here; a
-# Consumer should check for known types if it matters.
-# Interpretation = Literal["count", "mean"] - left as a generic string so it
-# can be added to.
-Interpretation = str
+# Known kinds of histograms. A Producer can add Kinds not defined here; a
+# Consumer should check for known types if it matters. A simple plotter could
+# just use .value and .variance if non-None and ignore .kind.
+#
+# Could have been Kind = Literal["COUNT", "MEAN"] - left as a generic string so
+# it can be extendable.
+Kind = str
 
 # Implementations are highly encouraged to use the following construct:
-# class Interpretation(str, enum.Enum):
-#     count = "count"
-#     mean = "mean"
+# class Kind(str, enum.Enum):
+#     COUNT = "COUNT"
+#     MEAN = "MEAN"
+# Then return and use Kind.COUNT or Kind.MEAN.
 
 @runtime_checkable
 class PlottableTraits(Protocol):
@@ -53,7 +56,12 @@ T = TypeVar("T", covariant=True)
 
 @runtime_checkable
 class PlottableAxisGeneric(Protocol[T]):
+    # name: str - Optional, not part of Protocol
     # label: str - Optional, not part of Protocol
+    #
+    # Plotters are encouraged to plot label if it exists and is not None, and
+    # name otherwise if it exists and is not None, but these properties are not
+    # available on all histograms and not part of the Protocol.
 
     traits: PlottableTraits
 
@@ -101,21 +109,33 @@ class PlottableHistogram(Protocol):
 
     def variances(self) -> Optional[ArrayLike]:
         """
-        Variance returns the variance of the values (for "mean" histograms,
+        Variance returns the variance of the values (for "MEAN" histograms,
         this is not the variance of the counts, but the variance of the mean).
         If unweighed, this returns None.
 
-        If counts is less than 2, the variance in that cell is undefined if
-        interpretation=="mean".
+        If counts is equal to 1 or less, the variance in that cell is undefined if
+        kind=="MEAN".
         """
 
-    def counts(self) -> ArrayLike:
+    def counts(self) -> Optional[ArrayLike]:
         """
-        Count returns the number of values counted in a mean accumulator (also
-        known as a Profile histogram), or is identical to .values if
-        interpretation is "count".
+        Count returns the effective number of entries. Current Kinds of common histograms
+        all have a defined counts, but an exotic histogram could have a None .counts, so
+        this is Optional and should be checked by Consumers.
+
+        For a weighted histogram, counts is defined as sum_of_weights ** 2 /
+        sum_of_weights_squared. It is always equal to or smaller than the
+        number of times the histogram was filled. If there is no variance in
+        the weights, this is exactly equal to the number of times this was
+        filled. The larger the spread in weights, this is smaller, but always 0
+        if filled 0 times, and 1 if filled once, and more than 1.
+
+        A suggested implementation is:
+
+            return np.divide(
+                sum_of_weights**2,
+                sum_of_weights_squared,
+                out=np.zeros_like(sum_of_weights, dtype=np.float64),
+                where=sum_of_weights_squared!=0)
         """
 
-    # This version of the protocol does not define "counts_variance" - but if
-    # included, it should have that name for forward compatibility with future
-    # versions of this Protocol.
