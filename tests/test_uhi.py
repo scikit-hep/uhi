c2d17f9e94a5b7e1074440b3d785ed8509214219
Henry Schreiner <henryschreineriii@gmail.com>
2021-03-09 10:07:41 -0500

    fix: use tests to verify verison and metadata version match

--- tests/test_uhi.py
+++ tests/test_uhi.py
@@ -1,5 +1,11 @@
 from uhi import __version__
+import sys
+
+if sys.version_info < (3, 8):
+    import importlib_metadata as metadata
+else:
+    from importlib import metadata
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.0"
+    assert __version__ == metadata.version("uhi")
