e99f90873db58cb8aeaeb766683c2f702125d6a8
Henry Schreiner <henryschreineriii@gmail.com>
2025-04-25 13:48:51 -0400

    tests: more tests for schema
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/uhi/schema.py
+++ src/uhi/schema.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import functools
 import json
 import sys
+from collections.abc import Callable
 from pathlib import Path
-
-import fastjsonschema
+from typing import Any
 
 if sys.version_info < (3, 9):
     import importlib_resources as resources
@@ -13,17 +14,47 @@ else:
 
 histogram_file = resources.files("uhi") / "resources/histogram.schema.json"
 
-with histogram_file.open(encoding="utf-8") as f:
-    histogram_schema = fastjsonschema.compile(json.load(f))
+__all__ = ["histogram_file", "validate"]
 
 
-def validate(path: str | Path) -> None:
-    path = Path(path)
-    with path.open(encoding="utf-8") as f:
-        example = json.load(f)
+def __dir__() -> list[str]:
+    return __all__
 
-    histogram_schema(example)
+
+@functools.lru_cache(maxsize=None)
+def _histogram_schema() -> Callable[[dict[str, Any]], None]:
+    import fastjsonschema
+
+    with histogram_file.open(encoding="utf-8") as f:
+        return fastjsonschema.compile(json.load(f))  # type: ignore[no-any-return]
+
+
+def validate(data: dict[str, Any]) -> None:
+    """Validate a histogram object against the schema."""
+    validate = _histogram_schema()
+    validate(data)
+
+
+def main(*files: str) -> None:
+    """Validate histogram files."""
+    import fastjsonschema
+
+    retval = 0
+
+    for file in files:
+        with Path(file).open(encoding="utf-8") as f:
+            data = json.load(f)
+        try:
+            validate(data)
+        except fastjsonschema.JsonSchemaValueException as e:
+            print(f"ERROR {file}: {e.message}")  # noqa: T201
+            retval = 1
+        else:
+            print(f"OK {file}")  # noqa: T201
+
+    if retval:
+        raise SystemExit(retval)
 
 
 if __name__ == "__main__":
-    validate(*sys.argv[1:])
+    main(*sys.argv[1:])
