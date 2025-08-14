6230e8777f2df5de008ad5b67e22c6a7f6448903
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2025-08-13 22:54:08 -0400

    tests: add hdf5 and zip tests (#191)
    * tests: add hdf5 and zip tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix(types): use general dict a bit more
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
 
+import importlib.metadata
 import json
 from pathlib import Path
 
+import packaging.version
 import pytest
 
 import uhi.io.json
 
 h5py = pytest.importorskip("h5py", reason="h5py is not installed")
 uhi_io_hdf5 = pytest.importorskip("uhi.io.hdf5")
+
+BHVERSION = packaging.version.Version(importlib.metadata.version("boost_histogram"))
+HISTVERSION = packaging.version.Version(importlib.metadata.version("hist"))
 
 DIR = Path(__file__).parent.resolve()
 
@@ -70,3 +75,50 @@ def test_reg_load(tmp_path: Path) -> None:
 
     assert one["storage"]["type"] == "int"
     assert one["storage"]["values"] == pytest.approx([1, 2, 3, 4, 5])
+
+
+@pytest.mark.skipif(
+    packaging.version.Version("1.6.1") > BHVERSION,
+    reason="Requires boost-histogram 1.6+",
+)
+def test_convert_bh(tmp_path: Path) -> None:
+    import boost_histogram as bh
+
+    h = bh.Histogram(
+        bh.axis.Regular(3, 13, 10, __dict__={"name": "x"}), storage=bh.storage.Weight()
+    )
+    tmp_file = tmp_path / "test.h5"
+    with h5py.File(tmp_file, "w") as h5_file:
+        uhi_io_hdf5.write(h5_file.create_group("histogram"), h)
+
+    with h5py.File(tmp_file, "r") as h5_file:
+        rehist = uhi_io_hdf5.read(h5_file["histogram"])
+
+    h2 = bh.Histogram(rehist)
+
+    assert h == h2
+
+
+@pytest.mark.skipif(
+    packaging.version.Version("1.6.1") > BHVERSION
+    or packaging.version.Version("2.9.0") > HISTVERSION,
+    reason="Requires boost-histogram 1.6+ / Hist 2.9+",
+)
+def test_convert_hist(tmp_path: Path) -> None:
+    import hist
+
+    h = hist.Hist(
+        hist.axis.Regular(10, 0, 1, name="a", label="A"),
+        hist.axis.Integer(7, 13, overflow=False, name="b", label="B"),
+        storage=hist.storage.Weight(),
+        name="h",
+        label="H",
+    )
+    tmp_file = tmp_path / "test.h5"
+    with h5py.File(tmp_file, "w") as h5_file:
+        uhi_io_hdf5.write(h5_file.create_group("histogram"), h)
+
+    with h5py.File(tmp_file, "r") as h5_file:
+        rehist = uhi_io_hdf5.read(h5_file["histogram"])
+    h2 = hist.Hist(rehist)
+    assert h == h2
